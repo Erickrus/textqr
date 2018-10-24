84b31e01cc80bcd510aee853c1624fb36b53fad9
Erickrus <hyinghao@hotmail.com>
2018-10-24 13:40:32 +0800

    fixed issue in display half pix mode and added banner

--- textqr.py
+++ textqr.py
@@ -6,18 +6,20 @@ from PIL import Image, ImageDraw, ImageFont
 
 class TextQRCode:
   def __init__(self):
-    self.bgBlack="\033[40m"
-    self.bgWhite="\033[47m"
-    self.fgBlack="\033[0;30m"
-    self.fgWhite="\033[0;37m"
     self.colorOff="\033[0m"
     self.colorMapping = {
-      0: self.colorOff + " ",
-      1: self.bgWhite + self.fgBlack + "▄",
-      2: self.colorOff + "▄",
-      3: self.colorOff + "█"
+      0: "\033[0m" + " ",
+      1: "\033[30;47m" + "▄",
+      2: "\033[0m" + "▄",
+      3: "\033[0m" + "█"
     }
     self.pixelAcceptance = 64
+
+    self.qr = qrcode.QRCode(
+      version=1,
+      box_size=2,
+      border=1
+    )
 
   def drawRow(self, row):
     line = ""
@@ -63,23 +65,62 @@ class TextQRCode:
     self.draw(res)
 
   def draw_qrcode(self, text):
-    qr = qrcode.QRCode(
-      version=1,
-      box_size=2,
-      border=1
-    )
-    qr.add_data(text)
-    qr.make(fit=True)
-    im = qr.make_image(fill="black", back_color="white")
+    self.qr.add_data(text)
+    self.qr.make(fit=True)
+    im = self.qr.make_image(fill="black", back_color="white")
     whiteColor = (255, 255, 255)
     blackColor = (0,0,0)
+    im = im.resize((im.size[0]//2, im.size[1]//2), Image.BICUBIC)
     self.draw_im(im)
     del im
+
+  def _banner_ch(self, text):
+    whiteColor = (255, 255, 255)
+    blackColor = (0,0,0)
+    size = 9
+    im = Image.new ("RGB", (size, int(size*1.5)), whiteColor)
+    draw = ImageDraw.Draw(im)
+    draw.text((0,0), text, fill=blackColor)
+    res = ""
+    for y in range(int(size*1.5)):
+      for x in range(size):
+        p = im.getpixel((x,y))
+        if p[0] ==0:
+          res += "#"
+        else:
+          res += " "
+      res += "\n"
+    del im
+    return res
+
+  def banner(self, text):
+    chars = []
+    for i in range(len(text)):
+      ch = self._banner_ch(text[i])
+      lines = ch.split("\n")
+      if len(chars) ==0:
+        chars.extend(lines)
+      else:
+        for j in range(len(lines)):
+          chars[j]+= lines[j]
+
+    res = []
+    for j in range(len(chars)):
+      row = []
+      for k in chars[j]:
+        if k == "#":
+          row.append(1)
+        else:
+          row.append(0)
+      if len(row)>0:
+        res.append(row)
+    self.draw(res)
 
 if __name__ == "__main__":
   if len(sys.argv) >1:
     argv = sys.argv[1:]
     line = " ".join(argv)
     t = TextQRCode()
+    # t.banner(line)
     t.draw_qrcode(line)
 
